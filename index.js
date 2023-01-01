a3aea87219fd0c33bff1c5acc43469564818547a
NikkelM <nikkel.mollenhauer@student.hpi.de>
2023-01-01 13:30:41 +0100

    Better structure

--- index.js
+++ index.js
@@ -34,60 +34,51 @@ if (!fs.existsSync('./output')) {
 	fs.mkdirSync('./output');
 }
 
-// ---------- Fetch Game Pass game Id's ----------
+// ---------- Main ----------
+main();
 
-// Get all Game Pass Game Id's for this market
-console.log("Fetching Game Pass Id's for market " + CONFIG.market + ".\n");
+async function main() {
+	// Fetch Game Pass game ID's and properties for each pass type
+	// We do not await execution of these functions, as they are independent of each other
+	if (CONFIG.fetchConsole) {
+		runScriptForPassType("console");
+	}
+	if (CONFIG.fetchPC) {
+		runScriptForPassType("pc");
+	}
+	if (CONFIG.fetchEAPlay) {
+		runScriptForPassType("eaPlay");
+	}
+}
 
-let consoleGamePassIds, pcGamePassIds, eaPlayGamePassIds;
+async function runScriptForPassType(passType) {
+	// Fetch Game Pass game ID's
+	const gameIds = await fetchGameIDs(passType);
 
-if (CONFIG.fetchConsole) {
-	console.log("Fetching Console Game Pass Id's...");
-	consoleGamePassIds = await fetch(`https://catalog.gamepass.com/sigls/v2?id=f6f1f99f-9b49-4ccd-b3bf-4d9767a77f5e&language=en-us&market=${CONFIG.market}`)
+	// Fetch Game Pass game properties
+	await fetchGameProperties(gameIds, passType);
+}
+
+// ---------- Fetch Game Pass game ID's ----------
+async function fetchGameIDs(passType) {
+	// Get all Game Pass Game ID's for this market
+
+	const APIIds = {
+		"console": "f6f1f99f-9b49-4ccd-b3bf-4d9767a77f5e",
+		"pc": "fdd9e2a7-0fee-49f6-ad69-4354098401ff",
+		"eaPlay": "b8900d09-a491-44cc-916e-32b5acae621b"
+	}
+
+	console.log("Fetching " + passType + " Game Pass game ID's for market \"" + CONFIG.market + "\".");
+	let gameIds = await fetch(`https://catalog.gamepass.com/sigls/v2?id=${APIIds[passType]}&language=en-us&market=${CONFIG.market}`)
 		.then((response) => response.json())
 		.then((data) => data.filter((entry) => entry.id).map((entry) => entry.id));
-	console.log("Found " + consoleGamePassIds.length + " Game Pass games for Console.\n");
+
+	return gameIds;
 }
-
-if (CONFIG.fetchPC) {
-	console.log("Fetching PC Game Pass Id's...");
-	pcGamePassIds = await fetch(`https://catalog.gamepass.com/sigls/v2?id=fdd9e2a7-0fee-49f6-ad69-4354098401ff&language=en-us&market=${CONFIG.market}`)
-		.then((response) => response.json())
-		.then((data) => data.filter((entry) => entry.id).map((entry) => entry.id));
-	console.log("Found " + pcGamePassIds.length + " Game Pass games for PC.\n");
-}
-
-if (CONFIG.fetchEAPlay) {
-	console.log("Fetching PC Game Pass Id's...");
-	eaPlayGamePassIds = await fetch(`https://catalog.gamepass.com/sigls/v2?id=b8900d09-a491-44cc-916e-32b5acae621b&language=en-us&market=${CONFIG.market}`)
-		.then((response) => response.json())
-		.then((data) => data.filter((entry) => entry.id).map((entry) => entry.id));
-	console.log("Found " + eaPlayGamePassIds.length + " Game Pass games through EA Play.\n");
-}
-
-// ---------- Fetch Game Pass game properties ----------
-
-console.log("Fetching properties for all Game Pass games...\n");
-
-if (CONFIG.fetchConsole) {
-	console.log("Fetching Console Game Pass game properties...");
-	await fetchGameProperties(consoleGamePassIds, "console");
-}
-
-if (CONFIG.fetchPC) {
-	console.log("Fetching PC Game Pass game properties...");
-	await fetchGameProperties(pcGamePassIds, "pc");
-}
-
-if (CONFIG.fetchEAPlay) {
-	console.log("Fetching EA Play Game Pass game properties...");
-	await fetchGameProperties(eaPlayGamePassIds, "eaPlay");
-}
-
-// ---------- Fetch game properties ----------
 
 async function fetchGameProperties(gameIds, passType) {
-	console.log("Fetching game properties for " + gameIds.length + " " + passType + " games...\n");
+	console.log("Fetching game properties for " + gameIds.length + " " + passType + " games...");
 	await fetch(`https://displaycatalog.mp.microsoft.com/v7.0/products?bigIds=${gameIds}&market=${CONFIG.market}&languages=en-us&MS-CV=DGU1mcuYo0WMMp`)
 		.then((response) => response.json())
 		.then((data) => {
